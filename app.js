f129ce2112facfebc744afaa6a4e1e644b4d3c04
pcortezj <student.patronj@gmail.com>
2018-03-16 02:15:18 -0400

    linked db

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/status');
+mongoose.connect('mongodb://pcortezj:megatron2011@ds147668.mlab.com:47668/status');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
